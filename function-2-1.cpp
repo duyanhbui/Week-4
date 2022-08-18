5cc6b654c954f9360978cbaa13657802ed0189f1
duyanhbui <duyanh.bui@student.adelaide.edu.au>
2022-08-18 16:26:39 +0930

    haizz

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -3,13 +3,9 @@
 
 void multiples_of_seven(int *nums,int length){
     for(int i = 0; i< length; i++){
-        int abs_value = 0;
-        abs_value = std :: abs(*(nums+i));
-        if (abs_value == 1 or abs_value == 7 or abs_value == 9 ){
-            *(nums+i) *= 7;
-            std :: cout << *(nums+i) << std :: endl;
+        if (*(nums + i) % 7 == 0){
+            std :: cout << *(nums + i) << std :: endl;
         }
-
     }
 
 }
